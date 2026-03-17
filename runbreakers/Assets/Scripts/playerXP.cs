70acf36cda573f5a13e8694799c8742b95b1b600
WhiteLotus1998 <Roneycorey@gmail.com>
2026-03-17 14:58:21 -0400

    Updated XP system and working on Enemy Mage prefab
    I'm still getting the Enemy Mage working but I added a XP update in the meantime.

--- runbreakers/Assets/Scripts/playerXP.cs
+++ runbreakers/Assets/Scripts/playerXP.cs
@@ -5,6 +5,7 @@ public class playerXP : MonoBehaviour
     [Header("---- XP ----")]
     [SerializeField] int currentXP;
     [SerializeField] int currentLevel = 1;
+    [SerializeField] int maxXP = 100;
 
     public int GetCurrentXP()
     {
@@ -19,6 +20,28 @@ public class playerXP : MonoBehaviour
     public void AddXP(int amount)
     {
         currentXP += amount;
-        Debug.Log("Player gained " + amount + "XP. Total XP: " + currentXP);
+        Debug.Log("Player gained " + amount + " XP. Total XP: " + currentXP);
+
+        CheckLevelUp();
+    }
+
+    void CheckLevelUp()
+    {
+        while (currentXP >= maxXP)
+        {
+            currentXP -= maxXP; // carry over extra XP
+            currentLevel++;
+
+            Debug.Log("Player Leveled up! New Level: " + currentLevel);
+
+            IncreaseXPThreshold();
+        }
+    }
+
+    void IncreaseXPThreshold()
+    {
+        maxXP = Mathf.RoundToInt(maxXP * 1.2f);
+
+        Debug.Log("Next level requires " + maxXP + " XP");
     }
 }
