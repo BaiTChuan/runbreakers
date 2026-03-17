f6097774a85ea5c1fbfad4f52f3994e873366cfa
FrankThant <STThant@student.fullsail.edu>
2026-03-17 15:57:09 -0400

    Health Buff, IBuff, BuffStat, BuffPickup

---
+++ runbreakers/Assets/Scripts/buffStats.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class buffStats : ScriptableObject
+{
+    public GameObject buffModel;
+    [Header("----- BuffType ------")]
+    [Header(" 0 = Health | 1 = Speed | 2 = Damage ")]
+    [Range (0 , 2)] public int id;
+
+    [Header("----- HealthBuff ------")]
+    public int healAmount;
+
+    [Header("----- SpeedBuff ------")]
+    public float speedMultiplier;
+    public int speedDuration;
+
+    [Header("----- DamageBuff ------")]
+    public float damageMultiplier;
+    public int damageDuration;
+}
